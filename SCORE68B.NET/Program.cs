e09037993ad5d52f68aa981ca81c6cc3b723f890
Chris Mallery <zoggins@retro-spy.com>
2020-11-23 21:13:42 -0800

    added another song and made some improvements to the C# score generation.

--- SCORE68B.NET/Program.cs
+++ SCORE68B.NET/Program.cs
@@ -5,8 +5,23 @@ namespace Score68B.Net
 {
     class Program
     {
+        static void GenerateSREC(int startAddress, byte[] byteArray, int numBytes)
+        {
+            int numFullRecords = numBytes / 32;
+            for (int i = 0; i < numFullRecords; ++i)
+            {
+                Console.Write(string.Format("S123{0:X4}", startAddress));
+                for(int j = 0; j < 32; ++j)
+                {
+                    //byteArray[(i*32)+j]
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            byte[] byteArray = new byte[1000];
+
             if (args.Length < 1)
             {
                 Console.WriteLine(string.Format("Usage: Score68B.Net.exe <input file> [Tempo Value]", args[0]));
@@ -32,14 +47,15 @@ namespace Score68B.Net
                 Console.WriteLine(string.Format("Couldn't open {0} for reading.", args[0]));
                 return;
             }
+            var v = 1;
 
             using (StringReader reader = new StringReader(data))
             {
-                var v = 0;
+
                 string line = string.Empty;
 
                 do
-                {
+                { 
                     line = reader.ReadLine();
                     if (line != null)
                     {
@@ -71,6 +87,11 @@ namespace Score68B.Net
                                 double t = 100;
 
                                 var trimmedNote = note.Trim();
+                                if (trimmedNote.Length < 4 && !(trimmedNote.Length == 1 && trimmedNote[0] == 'X'))
+                                {
+                                    ReportError(v, trimmedNote, c);
+                                    return;
+                                }
 
                                 var a = trimmedNote.Substring(0, 1)[0];
                                 var e = 100;
@@ -134,7 +155,14 @@ namespace Score68B.Net
                                         n = 11;
                                         break;
                                     case 'X':
-                                        Console.WriteLine("0");
+                                        byteArray[3 * (v - 1)] = 0;
+                                        for (int i = 0; i < ((v - 1) * 3)+1; ++i)
+                                        {
+                                            Console.Write(string.Format("{0:X2}", byteArray[i]));
+                                            //Console.Write(" ");
+                                            //if ((i+1) % 3  == 0)
+                                            //    Console.Write("\n");
+                                        }
                                         return;
                                     default:
                                         ReportError(v, trimmedNote, c);
@@ -209,16 +237,19 @@ namespace Score68B.Net
                                         t = 2 * t / 3;
                                 }
                                 double f1 = 220.0 * (Math.Pow(k1, (p - 1)));
-                                double t1 = Math.Pow(10.0, 6.0 / (2.0 * f1));
+                                double t1 = Math.Pow(10.0, 6.0) / (2.0 * f1);
                                 double k3 = (t1 * 1.7971 / 2.0 - 185.0) / 6.0;
                                 double k4 = f1 / (k6 * t);
                                 int d3 = Convert.ToInt32(k4);
-                                int d4 = 2 * d3 - 2 * Convert.ToInt32(d3 / 2);
+                                int d4 = 2 * d3 - 2 * Convert.ToInt32(d3 / 2.0);
                                 var d5 = Convert.ToInt32(d4 / 256.0);
                                 int d6 = d5 + e1;
                                 int d7 = d4 - d5 * 256;
 
-                                Console.WriteLine("{0:X2} {1:X2} {2:X2}", (byte)(Convert.ToInt32(k3 + .5)), d6, d7);
+                                //Console.WriteLine("{0:X2} {1:X2} {2:X2}", (Convert.ToInt32(Math.Floor(k3 + .5))), d6, d7);
+                                byteArray[3 * (v - 1)] = (byte)Convert.ToInt32(Math.Floor(k3 + .5));
+                                byteArray[3 * (v - 1) + 1] = (byte)d6;
+                                byteArray[3 * (v - 1) + 2] = (byte)d7;
 
                                 v++;
                             }
@@ -227,7 +258,6 @@ namespace Score68B.Net
                     }
                 } while (line != null) ;
             }
-
         }
 
         private static void ReportError(int v, string trimmedNote, int c)
